4cc23cedf062c439e7ba2202e98c5e8160225d9e
yana.pleshachkova <yana.pleshachkova@gmail.com>
2017-03-12 21:17:54 +0500

    1. Improved game initialization

--- Taken/Program.cs
+++ Taken/Program.cs
@@ -14,65 +14,44 @@ namespace Taken
 
         private int zeroX, zeroY; // Для сохранения координат 0
 
-        public Game(
-            int i1, int i2, int i3,
-            int i4, int i5, int i6,
-            int i7, int i8, int i9) // Поле 3*3
+        public Game(string path)
         {
-            area = new int[3, 3]; // Инициализируем поле (задаем размеры)
+            string[] lines = System.IO.File.ReadAllLines(@path);
 
-            area[0, 0] = i9;
-            zeroX = 0;
-            zeroY = 0;
+            string[] array = lines[0].Split(',');
 
-            area[0, 1] = i1;
-            area[0, 2] = i2;
+            if (!(Math.Sqrt(array.Length) % 1 == 0))
+            {
+                lenX = null;
+                lenY = null;
 
-            area[1, 0] = i3;
-            area[1, 1] = i4;
-            area[1, 2] = i5;
+                Log.Message("Задано некорректное поле дляигры.");
+            }
+            else
+            {
+                lenX = lenY = (int)Math.Sqrt(array.Length);
 
-            area[2, 0] = i6;
-            area[2, 1] = i7;
-            area[2, 2] = i8;
+                Log.Message("Сторона игрового поля = " + lenX);
 
-            lenX = 3; // Длина X
-            lenY = 3; // Длина Y
-        }
+                area = new int[(int)lenX, (int)lenY]; // Инициализируем поле (задаем размеры)
+                int k = 0;
 
-        public Game(
-            int i1, int i2, int i3, int i4,
-            int i5, int i6, int i7, int i8,
-            int i9, int i10, int i11, int i12,
-            int i13, int i14, int i15, int i16) // Поле 4*4
-        {
-            area = new int[4, 4];
+                for (int i = 0; i < lenX; i++)
+                {
+                    for (int j = 0; j < lenY; j++)
+                    {
+                        area[i, j] = int.Parse(array[k]);
 
-            area[0, 0] = i16;
-            zeroX = 0;
-            zeroY = 0;
+                        if (area[i, j] == 0)
+                        {
+                            zeroX = i;
+                            zeroY = j;
+                        }
 
-            area[0, 1] = i1;
-            area[0, 2] = i2;
-            area[0, 3] = i3;
-
-            area[1, 0] = i4;
-            area[1, 1] = i5;
-            area[1, 2] = i6;
-            area[1, 3] = i7;
-
-            area[2, 0] = i8;
-            area[2, 1] = i9;
-            area[2, 2] = i10;
-            area[2, 3] = i11;
-
-            area[3, 0] = i12;
-            area[3, 1] = i13;
-            area[3, 2] = i14;
-            area[3, 3] = i15;
-
-            lenX = 4;
-            lenY = 4;
+                        k++;
+                    }
+                }
+            }
         }
 
         private int GetValue(int x, int y) // получить значение по координатам
@@ -145,7 +124,8 @@ namespace Taken
     {
         static void Main(string[] args)
         {
-            Game game = new Game(1, 2, 3, 4, 5, 6, 7, 8, 0); // создали экземпляр класса с полем 3*3
+            string PATH_TO_GAME = "C:\\Users\\acer\\Documents\\visual studio 2015\\Projects\\Taken\\Game.txt";
+            Game game = new Game(PATH_TO_GAME); // создали экземпляр класса с полем 3*3
 
             game.Shift(0); // Попробуем переместить пустую клетку
 
